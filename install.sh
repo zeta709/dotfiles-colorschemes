f8f8a5f8ed78ba2bd600970d709918f77340ce60
zeta709 <zeta709@gmail.com>
2022-01-31 14:55:59 +0900

    Fix install.sh (file existence check)

--- install.sh
+++ install.sh
@@ -8,13 +8,13 @@ install() (
 	cd "$scriptpath" || return 1
 
 	file="${scriptpath}/sh/.term.sh"
-	[ ! -f "$file" ] && ln -s "/dev/null" "$file"
+	[ ! -e "$file" ] && ln -s "/dev/null" "$file"
 	if ! grep -Fq ".term.sh" "${HOME}/.zshrc"; then
 		printf 'source "%s"\n' "$file" >> "${HOME}/.zshrc"
 	fi
 
 	file="${scriptpath}/tmux/.term.tmux.conf"
-	[ ! -f "$file" ] && ln -s "/dev/null" "$file"
+	[ ! -e "$file" ] && ln -s "/dev/null" "$file"
 	if ! grep -Fq ".term.tmux.conf" "${HOME}/.tmux.conf"; then
 		printf 'source "%s"\n' "$file" >> "${HOME}/.tmux.conf"
 	fi
