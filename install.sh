d0f2476aef8b78052fb9fe51693f37216b270cfb
zeta709 <zeta709@gmail.com>
2022-02-05 17:00:17 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,32 +2,39 @@
 # vim: noexpandtab:sw=4:ts=4
 
 # NOTE: this way to get the scriptdir is not perfect
+# TODO: escape special characters in the scriptdir
 scriptdir=$(unset CDPATH && cd -- "$(dirname -- "$0")" && pwd -P)
+
+# do_source_cmd: insert the source command into the rc_file
+# usage: do_source_cmd "$color_file" "$rc_file"
+do_source_cmd() {
+	color_file=$1
+	rc_file=$2
+	[ ! -e "$color_file" ] && ln -s "/dev/null" "${scriptdir}/${color_file}"
+	if ! grep -Fq "$color_file" "$rc_file"; then
+		printf 'source "%s"\n' "${scriptdir}/${color_file}" >> "$rc_file"
+		printf "Installed: '%s' in '%s'\n" $color_file $rc_file
+	else
+		printf "Skip: '%s' for '%s'\n" $color_file $rc_file
+	fi
+}
 
 install() (
 	cd "$scriptdir" || return 1
 
-	file="${scriptdir}/sh/.term.sh"
-	[ ! -e "$file" ] && ln -s "/dev/null" "$file"
-	if ! grep -Fq ".term.sh" "${HOME}/.zshrc"; then
-		printf 'source "%s"\n' "$file" >> "${HOME}/.zshrc"
-	fi
+	# shell
+	[ -r "${HOME}/.bashrc" ] && do_source_cmd "sh/.term.sh" "${HOME}/.bashrc"
+	[ -r "${HOME}/.zshrc" ] && do_source_cmd "sh/.term.sh" "${HOME}/.zshrc"
 
-	file="${scriptdir}/tmux/.term.tmux.conf"
-	[ ! -e "$file" ] && ln -s "/dev/null" "$file"
-	if ! grep -Fq ".term.tmux.conf" "${HOME}/.tmux.conf"; then
-		printf 'source "%s"\n' "$file" >> "${HOME}/.tmux.conf"
-	fi
+	# tmux
+	do_source_cmd "tmux/.term.tmux.conf" "${HOME}/.tmux.conf"
+	do_source_cmd "tmux/.colors.tmux.conf" "${HOME}/.tmux.conf"
 
-	file="${scriptdir}/tmux/.colors.tmux.conf"
-	if ! grep -Fq ".colors.tmux.conf" "${HOME}/.tmux.conf"; then
-		printf 'source -q "%s"\n' "$file" >> "${HOME}/.tmux.conf"
-	fi
-
+	# vim
 	file="${scriptdir}/vim/.colors.vim"
 	[ ! -e "$file" ] && ln -s "/dev/null" "$file"
 	if ! grep -Fq ".colors.vim" "${HOME}/.vimrc"; then
-		printf "exec 'source' fnameescape('%s')\n" "$file" >> "${HOME}/.vimrc"
+		printf "exec 'source' fnameescape(\"%s\")\n" "$file" >> "${HOME}/.vimrc"
 	fi
 )
 
